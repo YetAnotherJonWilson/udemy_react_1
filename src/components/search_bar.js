24b37d2d272320da5a1163be0f14a2071d25e56a
Jon Wilson <jonwilsoncomedy@gmail.com>
2017-05-28 23:57:22 -0500

    adding event handler for searchbar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 class SearchBar extends React.Component {
     render() {
-        return <input />;
+        return <input onChange={e => console.log(e.target.value)} />;
     }
 }
 

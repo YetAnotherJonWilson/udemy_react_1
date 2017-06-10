a02097eca4c857af5b0c33673c549d84908a3163
Jon Wilson <jonwilsoncomedy@gmail.com>
2017-06-10 18:09:58 -0500

    refactoring app component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
@@ -6,19 +6,27 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCchcPqzfUXQnyHUWXLABtVoYwBrXW-isg';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-        console.log(data)
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+                this.setState({ videos });
+            }
+        );
     }
-);
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-};
+    render() {
+        return(
 
+            <div>
+                <SearchBar />
+            </div>
+        );
+    };
+}
 
 ReactDOM.render(
     <App />,
